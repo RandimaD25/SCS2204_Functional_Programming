d25f82d6b113dec05c6e2111d59e422e4e1427ff
Randima Dias <randimamethminid525@gmail.com>
2022-09-12 22:09:03 +0530

    Add tutorial 7

--- Tutorial/7/20000421/Q3.scala
+++ Tutorial/7/20000421/Q3.scala
@@ -1,46 +1,30 @@
-object Q3 extends App {
-    var accountList:List[Account] = List()
+object Q3 extends App{
 
-    def accCreate(nic:String, accId: Int):Unit = {
-        val acc = new Account(nic, accId)
-        accountList = accountList ::: acc :: Nil
-        println(accountList)
+  class Account(id: String, n: Int, b: Double){
+    val NIC:String = id
+    val acnumber:Int = n
+    var Balance:Double = b
+
+    def withdraw(a:Double) = this.Balance = this.Balance - a
+
+    def deposit(a:Double) = this.Balance = this.Balance + a
+
+    def transfer(a:Account, b:Double):Unit = {
+      this.withdraw(b)
+      a.deposit(b)
     }
+    override def toString = "NIC - " + this.NIC + "   Acc. NO - " + this.acnumber + "   Balance - " + this.Balance
+  }
 
-    val find = (a:Int, b:List[Account]) => b.filter(account => account.accId.equals(a))
+  val account1 = new Account("984550348v", 13367899, 60000)
+  val account2 = new Account("972749180v", 13324889, 25000)
 
-    /*              Driver Code                */
-    
-    accCreate("1",1)
-    accCreate("2",2)
+  println("Account before transfer money: ")
+  println(account1)
+  println(account2)
 
-    //deposit money
-    find(1, accountList)(0).deposit(1000)
-    println(find(1, accountList)(0))
-
-    //transfer money
-    find(1, accountList)(0).transfer(2, 100.0)
-    println(find(2, accountList)(0))
-}
-
-class Account(nic:String, val accId: Int, var balance: Double = 0.0){
-
-    def withdrow(amount:Double) : Unit = {
-        this.balance = this.balance - amount
-    }
-
-    def deposit(amount:Double) : Unit = {
-        this.balance = this.balance + amount
-    }
-
-    def transfer(account:Int, amount:Double) : Unit = {
-        val transferAcc = Q4.find(account, Q4.accountList)
-        if (balance < 0.0) println("Insufficient balance")
-        else {
-            this.withdrow(amount)
-            transferAcc(0).deposit(amount)
-        }
-    }
-
-    override def toString = "["+nic+":"+accId +":"+ balance+"]"
+  account1.transfer(account2, 15000)
+  println("\nAccount after transfer money: ")
+  println(account1)
+  print(account2)
 }
